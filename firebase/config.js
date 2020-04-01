7503922cacc7dc979a116e97d0e8993d89e34f58
Israel H <israelherrerae@gmail.com>
2020-03-31 20:40:54 -0500

    eliminando claves api XD

--- firebase/config.js
+++ firebase/config.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCTLG6W6OYgabIVuV_RHuZn2EYo1Pvl2FI",
-  authDomain: "pasteles-app.firebaseapp.com",
-  databaseURL: "https://pasteles-app.firebaseio.com",
-  projectId: "pasteles-app",
-  storageBucket: "pasteles-app.appspot.com",
-  messagingSenderId: "480513943003",
-  appId: "1:480513943003:web:b78d5ac265557cec1573fe"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 export default firebaseConfig;
