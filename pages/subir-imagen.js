d2a1985efea23bca895cbe18823932bdb82c511a
Israel H <israelherrerae@gmail.com>
2021-12-09 15:24:49 -0500

    update minors

--- pages/subir-imagen.js
+++ pages/subir-imagen.js
@@ -3,13 +3,13 @@ import firebase from "firebase";
 import FileUploader from "react-firebase-file-uploader";
 
 const config = {
-  apiKey: "AIzaSyCTLG6W6OYgabIVuV_RHuZn2EYo1Pvl2FI",
-  authDomain: "pasteles-app.firebaseapp.com",
-  databaseURL: "https://pasteles-app.firebaseio.com",
-  projectId: "pasteles-app",
-  storageBucket: "pasteles-app.appspot.com",
-  messagingSenderId: "480513943003",
-  appId: "1:480513943003:web:b78d5ac265557cec1573fe"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 //firebase.initializeApp(config);
 
